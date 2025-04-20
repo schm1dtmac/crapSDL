ab886ebcd141beee26c79942bf7f01fb5c1891c9
schm1dtmac <schm1dtxbox@icloud.com>
2025-04-20 19:29:46 +0100

    Update SDL_cocoawindow.m

--- src/video/cocoa/SDL_cocoawindow.m
+++ src/video/cocoa/SDL_cocoawindow.m
@@ -524,7 +524,7 @@ static SDL_bool AdjustCoordinatesForGrab(SDL_Window * window, int x, int y, CGPo
 
     /* you need to be FullScreenPrimary, or toggleFullScreen doesn't work. Unset it again in windowDidExitFullScreen. */
     [nswindow setCollectionBehavior:NSWindowCollectionBehaviorFullScreenPrimary];
-    [nswindow performSelectorOnMainThread: @selector(toggleFullScreen:) withObject:nswindow waitUntilDone:NO];
+    [nswindow performSelectorOnMainThread: @selector(toggleFullScreen:) withObject:nswindow waitUntilDone:YES];
     [nswindow setCollectionBehavior:NSWindowCollectionBehaviorManaged];
     return YES;
 }

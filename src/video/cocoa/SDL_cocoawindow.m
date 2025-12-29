bed45504b76dadb4c172acfc5cae57d26b3355ac
schm1dtmac <schm1dtxbox@icloud.com>
2025-12-29 18:39:03 +0000

    Update SDL_cocoawindow.m

--- src/video/cocoa/SDL_cocoawindow.m
+++ src/video/cocoa/SDL_cocoawindow.m
@@ -1516,7 +1516,7 @@ int Cocoa_CreateWindow(_THIS, SDL_Window * window)
     [contentView setSDLWindow:window];
 	[contentView setWantsExtendedDynamicRangeOpenGLSurface:YES];
 	[[contentView layer] setWantsExtendedDynamicRangeContent:YES];
-	[[contentView layer] setColorSpace:CGColorSpaceCreateWithName(kCGColorSpaceITUR_2020)];
+	[[contentView layer] setColorspace:CGColorSpaceCreateWithName(kCGColorSpaceITUR_2020)];
 
     /* We still support OpenGL as long as Apple offers it, deprecated or not, so disable deprecation warnings about it. */
     #ifdef __clang__
